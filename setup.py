68fa6f5eddf3755e5b435e13de9242e6abcd1f16
Claudio Facchinetti <devack@hotmail.it>
2019-02-13 18:12:49 +0100

    Added licence and setup files

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="twitchbot",
+    version="0.0.1",
+    author="Facco089",
+    author_email="devack@hotmail.it",
+    description="An easy library to create a twitch bot",
+    url="https://github.com/facco089/twitchbot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
